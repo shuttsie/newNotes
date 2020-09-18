8daf521f827a904f0d46b165d316bf0bcd047bf5
Ryan <shuttsie@gmail.com>
2020-09-18 09:01:01 -0500

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,25 @@
 import React from "react"
+import Layout from "../components/layout"
+import { Auth0Provider } from "@auth0/auth0-react"
+
+ReactDOM.render(
+  <Auth0Provider
+    domain="fiservseminars.us.auth0.com"
+    clientId="zRyYsoSjQEFqMqyZ61qQAJX2Ep2uQdSA"
+    redirectUri={window.location.origin}
+    audience="https://fiservseminars.us.auth0.com/api/v2/"
+    scope="read:current_user update:current_user_metadata"
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById("root")
+)
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <h1>Public Notes</h1>
+      <p>lk;jhlkjlkjlkjlkjlkjlkjljlkjlkjkjlhjiouoijklj</p>
+    </Layout>
+  )
 }
